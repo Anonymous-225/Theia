3963fdd45fa93a96bff9d6afc6630abcaa3457f4
Anonymous-225 <124226281+Anonymous-225@users.noreply.github.com>
2023-02-01 23:11:18 -0600

    Add files via upload

---
+++ Benchmark/Mutated/B5.py
@@ -0,0 +1,134 @@
+
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torchvision.transforms import ToTensor
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset, DataLoader
+from torchvision import datasets
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+train_data = datasets.FashionMNIST(
+    root = 'data',
+    train = True,                         
+    transform = transforms.Compose([transforms.ToTensor()]), 
+    download = True,            
+)
+print(train_data)
+test_data = datasets.FashionMNIST(
+    root = 'data', 
+    train = False, 
+    transform = transforms.Compose([transforms.ToTensor()])
+)
+
+
+train_loader = DataLoader(train_data, batch_size=100)
+test_loader = DataLoader(test_data, batch_size=100)
+print(train_loader)
+batch = next(iter(train_loader))
+images, labels = batch
+
+class FashionCNN(nn.Module):
+    
+    def __init__(self):
+        super(FashionCNN, self).__init__()
+        
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        
+        self.fc1 = nn.Linear(in_features=64*6*6, out_features=600)
+        self.drop = nn.Dropout2d(0.25)
+        self.fc2 = nn.Linear(in_features=600, out_features=120)
+        self.fc3 = nn.Linear(in_features=120, out_features=10)
+    
+        
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.drop(out)
+        out = self.fc2(out)
+        out = self.fc3(out)
+       
+        return out
+model = FashionCNN()
+model.to(device)
+
+error = nn.CrossEntropyLoss()
+
+learning_rate = 0.1
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+print(model)
+num_epochs = 5
+count = 0
+# Lists for visualization of loss and accuracy 
+loss_list = []
+iteration_list = []
+accuracy_list = []
+
+# Lists for knowing classwise accuracy
+predictions_list = []
+labels_list = []
+
+
+for epoch in range(num_epochs):
+    for images, labels in train_loader:
+        # Transfering images and labels to GPU if available
+        images, labels = images.to(device), labels.to(device)
+    
+        train = Variable(images.view(100, 1, 28, 28))
+        labels = Variable(labels)
+        
+        # Forward pass 
+        outputs = model(train)
+        loss = error(outputs, labels)
+        
+        # Initializing a gradient as 0 so there is no mixing of gradient among the batches
+        optimizer.zero_grad()
+        
+        #Propagating the error backward
+        loss.backward()
+        optimizer.step()
+    
+        count += 1
+    
+    # Testing the model
+    
+        if not (count % 50):    # It's same as "if count % 50 == 0"
+            total = 0
+            correct = 0
+        
+            for images, labels in test_loader:
+                images, labels = images.to(device), labels.to(device)
+                labels_list.append(labels)
+            
+                test = Variable(images.view(100, 1, 28, 28))
+            
+                outputs = model(test)
+            
+                predictions = torch.max(outputs, 1)[1].to(device)
+                predictions_list.append(predictions)
+                correct += (predictions == labels).sum()
+            
+                total += len(labels)
+            
+            accuracy = correct * 100 / total
+            loss_list.append(loss.data)
+            iteration_list.append(count)
+            accuracy_list.append(accuracy)
+        
+        if not (count % 500):
+            print("Iteration: {}, Loss: {}, Accuracy: {}%".format(count, loss.data, accuracy))
